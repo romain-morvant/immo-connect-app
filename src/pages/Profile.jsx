af1c67977236ff4709dc205dd82a7f93521437f1
Romain Morvant <romainmorvantdev@gmail.com>
2022-02-16 09:46:35 +0100

    Ajout de la déconnexion pour l'utilisateur connecté.

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,18 +1,33 @@
 import { useState, useEffect } from 'react';
 import { getAuth } from 'firebase/auth';
+import { useNavigate, Link } from 'react-router-dom';
 
 function Profile() {
-  const [user, setUser] = useState(null);
-
   const auth = getAuth();
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email
+  });
 
-  useEffect(() => {
-    setUser(auth.currentUser);
-  }, []);
+  const { name, email } = formData;
+
+  const navigate = useNavigate();
+
+  const onLogout = () => {
+    auth.signOut();
+    navigate('/');
+  };
 
 
   // Si l'utilisateur est connecté, j'affiche son nom, sinon j'informe le visiteur qu'il est introuvable
-  return user ? <h1>{user.displayName}</h1> : 'Utilisateur Introuvable ..';
+  return <div className='profile'>
+    <header className="profileHeader">
+      <p className="pageHeader"> Mon Profil</p>
+      <button type='button' className="logOut" onClick={onLogout}>
+        Logout
+      </button>
+    </header>
+  </div>;
 }
 
 export default Profile;
