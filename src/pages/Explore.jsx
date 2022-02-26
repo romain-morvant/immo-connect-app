67fa41faf9931e7c5c2ad33112e7acbafad599b6
Romain Morvant <romainmorvantdev@gmail.com>
2022-02-26 16:28:40 +0100

    Ajout du Slider sur la page d'accueil

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import Slider from "../components/Slider";
 import rentCategoryImage from '../assets/jpg/rentCategoryImage.jpg'
 import sellCategoryImage from '../assets/jpg/sellCategoryImage.jpg'
 
@@ -9,7 +10,7 @@ function Explore() {
         <p className="pageHeader">Quel type de bien recherchez vous ?</p>
       </header>
       <main>
-        {/* Slider */}
+        <Slider />
         <p className="exploreCategoryHeading">Categories</p>
         <div className="exploreCategories">
           <Link to={'/category/rent'}>
